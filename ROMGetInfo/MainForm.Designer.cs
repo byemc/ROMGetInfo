311d203d88d41fda1b27478c84f3e08457185eef
Bye <bye@byecorps.com>
2025-12-30 21:36:08 +0000

    Enable support for High-DPI displays

--- ROMGetInfo/MainForm.Designer.cs
+++ ROMGetInfo/MainForm.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.statusText = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
@@ -37,34 +36,42 @@
             // 
             // statusStrip1
             // 
-            resources.ApplyResources(this.statusStrip1, "statusStrip1");
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusText,
             this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 287);
             this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(462, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip";
             // 
             // statusText
             // 
-            resources.ApplyResources(this.statusText, "statusText");
             this.statusText.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(338, 20);
             this.statusText.Spring = true;
+            this.statusText.Text = "Ready.";
+            this.statusText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // toolStripProgressBar1
             // 
-            resources.ApplyResources(this.toolStripProgressBar1, "toolStripProgressBar1");
             this.toolStripProgressBar1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.toolStripProgressBar1.Margin = new System.Windows.Forms.Padding(1, 4, 8, 4);
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
             // 
             // MainForm
             // 
-            resources.ApplyResources(this, "$this");
             this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 313);
             this.Controls.Add(this.statusStrip1);
+            this.MinimumSize = new System.Drawing.Size(240, 360);
             this.Name = "MainForm";
+            this.Text = "MainForm";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
